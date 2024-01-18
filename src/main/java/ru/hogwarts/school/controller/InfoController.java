ba0ee0e907b77c8ed6efcba11c429a7d37dfcfc9
oleg-olegi <ol.shink87@gmail.com>
2024-01-19 00:46:42 +0300

    hw 4.4 step 3

--- src/main/java/ru/hogwarts/school/controller/InfoController.java
+++ src/main/java/ru/hogwarts/school/controller/InfoController.java
@@ -1,6 +1,7 @@
 package ru.hogwarts.school.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,12 +9,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/info")
+@Profile({"default", "dev", "prod"})
 public class InfoController {
+
     @Value("${server.port}")
     private String serverPort;
 
+    @Value("${info.app.name}")
+    private String appName;
+
     @GetMapping("/port")
-   public ResponseEntity<String> getPort() {
-        return ResponseEntity.ok("The application is running on port: " + serverPort);
+    public ResponseEntity<String> getServerPort() {
+        return ResponseEntity.ok("The application '" + appName + "' is running on port: " + serverPort);
     }
 }
